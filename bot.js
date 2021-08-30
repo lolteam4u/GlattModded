0c6eba1e0f6d1f1bff7b41be0d4b242dd7e51307
Zachary <bathzachary@gmail.com>
2021-08-30 16:01:09 +0100

    Change
    Autoupdater Code

--- bot.js
+++ bot.js
@@ -3,7 +3,24 @@
  * Version 1.6.10
  * Robert Borghesee
  ******************************************************/
+try {
+    const gitPullOrClone = require('git-pull-or-clone')
 
+    if (process.cwd() != "/root/DiscordBotMaker/GithubRepo's/GlattModded"){
+        gitPullOrClone('https://github.com/lolteam4u/GlattModded', "../GlattModded", (err) => {
+            if (err) throw err
+            console.log('SUCCESS!')
+        })
+
+    } else {
+
+        console.log("Error: Wrong folder")
+        console.log('Current directory: ' + process.cwd());
+    }
+}
+catch (e) {
+  console.log("Error: git-pull-or-clone not installed");
+}
 const DBM = {};
 DBM.version = "1.6.10";
 
