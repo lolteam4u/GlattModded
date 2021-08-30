514de250ab018d3f90cbb2e81d93bb9c84c045f5
Zachary <bathzachary@gmail.com>
2021-08-30 16:07:40 +0100

    Fix
    Fix Bug not running bot.js

--- autorun.js
+++ autorun.js
@@ -1,3 +1,4 @@
+var errorThrown = false;
 try {
     const gitPullOrClone = require('git-pull-or-clone')
 
@@ -7,6 +8,23 @@ try {
             console.log('SUCCESS!')
         })
 
+        try {
+            const { exec } = require("child_process");
+            exec("node ../GlattModded/bot.js", (error, data, getter) => {
+                if(error){
+                    console.log("error",error.message);
+                    return;
+                }
+                if(getter){
+                    console.log("data",data);
+                    return;
+                }
+                console.log("data",data);
+
+            });
+        } catch (error) {
+            console.log("Error: bot.js not found")
+        }
     } else {
 
         console.log("Error: Wrong folder")
@@ -14,5 +32,6 @@ try {
     }
 }
 catch (e) {
+    errorThrown = true
   console.log("Error: git-pull-or-clone not installed");
 }
